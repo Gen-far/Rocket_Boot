aafa88682c756a967d89b63ac4d9840bf6b36a5a
Gen-far <138238156+Gen-far@users.noreply.github.com>
2023-07-27 11:10:02 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# This file is for strategy
+
+from util.objects import *
+from util.routines import *
+from util.tools import find_hits
+
+class Bot(GoslingAgent):
+    # This function runs every in-game tick (every time the game updates anything)
+    def run(self):
+        if self.intent is not None:
+            return
+        if self.kickoff_flag:
+            self.set_intent(kickoff())
+            return
+        
+        if self.is_in_fornt_of_ball():
+            self.set_intent(goto(self.friend_goal_location))
+            return
+        
+        if self.me.boost > 99:
+            self.set_intent(short_shot(self.foe_goal.location))
+            return
+
+
+        if self.me.boost > 99:
+            self.set_intent(short_shot(self.foe_goal.location))
+            return
+        target_boost = self.get_closest_large_boost()
+
+        if target_boost is not None:
+            self.set_intent(goto(target_boost.location))
+            return
